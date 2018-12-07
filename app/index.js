50c7defda1faefd07f65c4e3a1f530cca607c8b2
Hugo STEPHAN <hugo.stephan@etu.u-pec.fr>
2018-12-07 14:43:25 +0100

    Ajout return

--- app/index.js
+++ app/index.js
@@ -10,38 +10,32 @@ var queue_name = 'lost' ;
 
 app.use(morgan('combined'));
 app.use(bodyParser.urlencoded({extended:true}));
-/*app.use(function (req, res, next) {
-    res.setHeader('Access-Control-Allow-Methods', 'GET, POST, OPTIONS, PUT, PATCH, DELETE');
-    res.setHeader('Access-Control-Allow-Headers', 'X-Requested-With,content-type');
-    res.setHeader('Access-Control-Allow-Credentials', true);
-    next();
-});*/
 
+// Root heartbeat
 app.get('/', (req, res) => {
 	res.sendFile(__dirname + '/heartbeat.json')
 });
 
+// POST /add_msg in queue
 app.post('/add_msg', (req, res) => {
 	var m = req.body.msg ;
 	queue_name = req.body.queue ;
-	//console.log(req);
 	amqp.connect(rabbit_host, function(err, conn) {
 	  conn.createChannel(function(err, ch) {
-		
-		//ch.assertQueue(queue_name, {durable: true});
 		ch.sendToQueue(queue_name, Buffer.from(m));
 		console.log(" [x] Message sent %s", m);
 	  });
 	});
 	res.send('add_message_success');
+	return ;
 });
 
+// GET /get_msg in queue
 app.get('/get_msg', (req, res) => {
 	var ret = "no_message";
 	queue_name = req.query.queue ;
 	amqp.connect(rabbit_host, function(err, conn) {
 	conn.createChannel(function(err, ch) {
-		//ch.assertQueue(queue_name, {durable: false});
 		console.log(" [*] Waiting for messages in %s. To exit press CTRL+C", queue_name);
 		ch.consume(queue_name, function(msg) {
 			console.log(" [x] Received %s", msg.content.toString());
@@ -50,8 +44,10 @@ app.get('/get_msg', (req, res) => {
 	});
 		setTimeout(function() { conn.close(); res.send("no_message")}, 500);
 	});
+	return ;
 });
 
+// HTTP listen point
 var listener = app.listen(process.env.PORT || 8080, function() {
  console.log('listening on port ' + listener.address().port);
 });
