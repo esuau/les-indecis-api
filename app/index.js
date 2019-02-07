e55e48d6fe96cbf6a2fdc4819d50e8bcb9c51e40
Hugo STEPHAN <hugo.stephan@etu.u-pec.fr>
2019-02-07 10:51:53 +0100

    Ajout log client list

--- app/index.js
+++ app/index.js
@@ -109,20 +109,23 @@ wsServer = new WebSocketServer({
 wsServer.on('request', function(request) {
     var connection = request.accept(null, request.origin);
     console.log((new Date()) + ' Connection accepted.');
-    connection.on('message', function(message) {
+	connection.on('message', function(message) 
+	{
 		if(message.indexOf('token:') != -1)
 		{
 			console.log("SAVING CLIENT WEBSOCKET");
 			var chars = message.split(':');
 			clients[chars[1]] = connection ;
-			console.log(clients);
+			console.log("CLIENT LIST : " + clients)
 		}
 		console.log("WEB SOCKET RECEIVED MESSAGE");
-        if (message.type === 'utf8') {
+        if (message.type === 'utf8') 
+		{
             console.log('Received Message: ' + message.utf8Data);
             connection.sendUTF(message.utf8Data);
         }
-        else if (message.type === 'binary') {
+        else if (message.type === 'binary') 
+		{
             console.log('Received Binary Message of ' + message.binaryData.length + ' bytes');
             connection.sendBytes(message.binaryData);
         }
